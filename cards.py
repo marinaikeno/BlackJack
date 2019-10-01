6f55a9b8bed4fe62e7cd7ea487b2f434cfaa2986
marinaikeno <marinaikeno@gmail.com>
2019-10-01 00:32:22 -0700

    first commit

---
+++ cards.py
@@ -0,0 +1,157 @@
+
+import random
+import copy
+suit_symbol = {'Hearts':u"\u2764", 'Diamonds':u"\u25C6", 'Clubs': u"\u2663", 'Spades': u"\u2660"}
+
+class Cards(object): # card class
+	def __init__(self, suits, numbers, values):
+		self.suits = suits
+		self.numbers = numbers
+		self.values = values
+
+class Deck(object): # deck class
+	def __init__(self):
+		self.cards = [] # empty list for deck of cards
+
+	def create(self): # creates all the cards 
+		suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+		numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+		for suit in suits: 
+			for number in numbers: 
+				values = None
+				if number == "K" or number == "J" or number  == "Q": 
+					values = 10
+				elif number == 'A':
+					values = 11 # default value of Ace is 11
+				else: 
+					values = number
+				self.cards.append(Cards(suit, number, values))
+	def shuffle(self): # shuffles cards
+		for x in range(0,51):
+			temp = self.cards[x]
+			rand = random.randint(0,51)
+			self.cards[x] = self.cards[rand]
+			self.cards[rand] = temp
+
+	def reset(self, player, dealer): # reset method that makes all the lists empty
+		player.hand = []
+		dealer.hand = []
+		self.cards = []
+
+	def deal(self, player, dealer): # deals cards to player and dealer
+		for i in range(0,4):
+			hand = self.cards.pop(0)
+			if i<2: 
+				player.hand.append(hand)
+			else: 
+				dealer.hand.append(hand)
+		player.add()
+		dealer.add()
+
+		suit_symbol = {'Hearts':u"\u2764", 'Diamonds':u"\u25C6", 'Clubs': u"\u2663", 'Spades': u"\u2660"}
+
+		card_list = []
+		for x in player.hand:
+			symbol = suit_symbol[x.suits]
+			
+			if x.numbers == 10: 
+				card_number = 10
+			else: 
+				card_number = str(x.numbers)+" "
+			card_list.append(card_number)
+			card_list.append(symbol)
+
+		print """
+ ____________	 ____________
+|            |	|            |
+| {}         |	| {}         |
+|            |	|            |
+|            |	|            |
+|            |	|            |
+|     {}     |	|     {}     |
+|            |	|            |
+|            |	|            |
+|            |	|            |
+|         {} |  |         {} |
+|____________|	|____________|
+			""".format(card_list[0], card_list[2], card_list[1].encode("utf-8")+' ', card_list[3].encode("utf-8")+' ', card_list[0], card_list[2])
+
+		print "\nYou drew \033[1;36m{} of {}\033[1;m and \033[1;36m{} of {}\033[1;m".format(player.hand[0].numbers, player.hand[0].suits, player.hand[1].numbers, player.hand[1].suits)
+
+class Player(object):
+	def __init__(self, name):
+		self.hand = []
+		self.sum = 0
+		self.name = name
+		self.chips = 100 # default number of chips is 100
+
+	def hit(self, deck):
+		hand1 = deck.cards.pop(0) # removes the card from the first index in the deck, and set variable hand1 to that removed card
+		self.hand.append(copy.deepcopy(hand1)) # appends a deep copy of the removed card into list 'hand'
+		self.add() # add() returns the sum, and passed to variable 'hand_sum'
+
+		# to print out ascii pic of card
+		symbol = suit_symbol[hand1.suits]
+		card_number=""
+		if hand1.numbers == 10: 
+			card_number = 10
+		else: 
+			card_number = str(hand1.numbers)+" "
+		print """
+		 ____________
+		|            |
+		| {}         |
+		|            |
+		|            |
+		|            |
+		|     {}     |
+		|            |
+		|            |
+		|            |
+		|         {} |
+		|____________|
+
+		""".format(card_number,symbol.encode("utf-8")+' ',card_number)
+		# print out what player drew
+		print "\n{} drew \033[1;36m{} of {}\033[1;m".format(self.name, hand1.numbers, hand1.suits)  
+		# to check if Ace is 11 or 1
+		for card in self.hand: # goes through all the cards in your hand 
+			if card.numbers == "A" and self.sum>21: # check if the card you have is an Ace, and if you sum is over 21
+				card.values = 1
+				self.add()
+				print "Whoops almost busted! We changed your Ace to 1."
+
+	def stay(self):
+		pass 
+
+	def add(self):
+		self.sum = 0
+		for i in self.hand: # not taking into consideration of Ace
+			self.sum+=int(i.values)
+		return self.sum
+
+class Dealer(Player): # inherits from Player, but hit method is altered
+	def __init__(self):
+		super(Dealer, self).__init__(name='~Ray the Dealer~') # setting default values
+	def hit(self,deck): # similar to hit from Player, but doens't print anything
+		hand1 = deck.cards.pop(0) # removes the card from the first index in the deck, and set variable hand1 to that removed card
+		self.hand.append(copy.deepcopy(hand1)) # appends a deep copy of the removed card into list 'hand'
+		self.add() # add() returns the sum, and passed to variable 'hand_sum'
+		print
+		# to check if Ace is 11 or 1
+		for card in self.hand: # goes through all the cards in your hand 
+			if card.numbers == "A" and self.sum>21: # check if the card you have is an Ace, and if you sum is over 21
+				card.values = 1
+				self.add()
+
+
+
+myDeck = Deck()
+myDeck.create()
+player1 = Player('marina')
+player2 = Player('bob')
+myDeck.deal(player1, player2)
+print player1.add()
+
+		
+
---
+++ play.py
@@ -0,0 +1,256 @@
+# coding: utf-8
+from cards import Cards, Deck, Player, Dealer
+
+blackjack = """\033[1;33m
+	__________.__                 __          ____.              __    
+	\______   \  | _____    ____ |  | __     |    |____    ____ |  | __
+	 |    |  _/  | \__  \ _/ ___\|  |/ /     |    \__  \ _/ ___\|  |/ /
+	 |    |   \  |__/ __ \   \___|    <  /\__|    |/ __ \   \___|    < 
+	 |______  /____(____  /\___  >__|_ \ \________(____  /\___  >__|_ \  
+	        \/          \/     \/     \/               \/     \/     \/  
+\033[1;m
+"""
+
+symbols= """\033[1;31m
+			                               _
+			       ,'`.    _  _    /\    _(_)_
+			      (_,._)  ( `' )  <  >  (_)+(_)
+			        /\     `.,'    \/      |
+\033[1;m
+"""
+print blackjack
+print symbols
+
+name = raw_input("What is your name? ") # Asks user what their name is 
+# chip = input("How many starter chips do you want? ") # ask user how many starter chips they want
+# while type(chip)!= int:  # if whatever they typed in is not an integer, it makes the user type again
+# 	chip = input("Please put an integer. Dummy. ")
+# bet = input("How much do you want to wager each time? (Please type an integer) ")
+# while type(bet) != int: 
+# 	bet = input("Please put an integer. Dummy. ")
+
+myDeck = Deck() # create Deck object
+player = Player(name) # create Player object
+dealer = Dealer() # create Dealer object
+
+def start_game(): # start game function
+	myDeck.create() # creates the deck
+	myDeck.shuffle() # shuffles the deck
+	myDeck.deal(player, dealer) # deals the cards to the players and prints message 
+
+def the_game(): # function that plays the game 
+	myDeck.reset(player, dealer) # every time the game starts/restarts, the cards are reset
+	start_game() # calls the start_game function and starts the game
+	print "Your total value is \033[1;33m{}\033[1;m".format(player.sum) # prints out total value
+
+	if dealer.sum == 21 and player.sum == 21: # if both dealer and player gets a black jack initially 
+		print "Double black jack! You tied!"
+		return # exits the game
+	elif dealer.sum == 21: # if the dealer gets a black jack
+		print "Dealer got black jack! Game ends. You a loser"
+		lose() # calls the lose function because you lost
+		return 
+	elif player.sum == 21: # if the player gets a black jack
+		print "You got a black jack! Game ends."
+		win() # calls the win function 
+		return
+
+	answer2 = raw_input("\nDo you want to hit or stay? (H = hit; S = stay)") # asks player if they want to hit or stay
+	while answer2.upper()!='H' and answer2.upper()!='S': 
+		answer2 = raw_input("Please type H for Hit and S for Stay: ")	
+	while answer2.upper() == 'H': # while the answer2 is "H" aka hit
+		player.hit(myDeck) # player hits 
+		print "Your total value is \033[1;33m{}\033[1;m".format(player.sum)
+
+		hand_sum = player.sum # equals the the total value of player
+		if hand_sum>21: # if the total is greater than 21
+			print "You're a Loser!"
+			lose() # you lose
+			return
+		else: # else 
+			answer2 = raw_input("\nDo you want to hit or stay? (H = hit; S = stay)") # prompts H/S question again
+			while answer2.upper()!='H' and answer2.upper()!='S': 
+				answer2 = raw_input("Please type H for Hit and S for Stay: ")	
+			# if user answers H, while loop will continue
+			# if user answer S, then exits the while loop and goes to next code
+	if answer2.upper() == 'S': # when the user answers S
+		player.stay() 
+		while(dealer.sum < 17): # 
+			dealer.hit(myDeck)
+		if dealer.sum > 21: 
+			print "Dealer bust at \033[1;31m{}\033[1;m! Player wins!".format(dealer.sum)
+			win()
+			return
+		elif dealer.sum > player.sum: 
+			print "Dealer wins \033[1;31m{}-{}\033[1;m. You lost. Loser".format(dealer.sum, player.sum)
+			lose()
+			return
+		elif dealer.sum < player.sum:
+			print "You win \033[1;31m{}-{}\033[1;m. You're not a loser. I guess".format(player.sum, dealer.sum)
+			win()
+			return
+		else: 
+			print "You tied \033[1;31m{}-{}\033[1;m. That's weird".format(player.sum, dealer.sum)
+			return
+def make_bet():
+	bet = None
+	while True: 
+		try: 
+			bet = int(input("\nHow many chips do you want to bet? (Please type an integer)"))
+		except:
+			print('\n Enter an integer. Dummy')
+		else:
+			break
+	return bet
+
+answer = raw_input("\nAre you ready to start the game? (Y/N)")
+while answer.upper()!='Y' and answer.upper()!='N': 
+	answer = raw_input("Please enter Y or N: ")
+while(answer.upper() == 'Y'):
+	print '\nYou have \033[1;35m{}\033[1;m chips'.format(player.chips)
+	# bet = input("\nHow many chips do you want to bet? (Please type an integer) ")
+	bet = make_bet()
+	def lose(): # function for when you lose
+		player.chips-=bet # you lose however much you bet
+		print """
+	 __   _____  _   _   _     ___  ____  _____     _______           _______  
+	 \ \ / / _ \| | | | | |   / _ \/ ___|| ____|   / /_   _|         |_   _\ \ 
+	  \ V / | | | | | | | |  | | | \___ \|  _|    | |  | |             | |  | |
+	   | || |_| | |_| | | |__| |_| |___) | |___   | |  | |             | |  | |
+	   |_| \___/ \___/  |_____\___/|____/|_____|  | |  |_|    _____    |_|  | |
+	                                               \_\       |_____|       /_/ 
+			"""
+		print "You lost \033[1;31m{}\033[1;m chips. You currently have \033[1;31m{}\033[1;m\n".format(bet, player.chips)
+		if player.chips<=0: # if loser run out of chips
+			print """
+	                              )                 
+	 (                         ( /(                 
+	 )\ )      )    )     (    )\())  )     (  (    
+	(()/(   ( /(   (     ))\  ((_)\  /((   ))\ )(   
+	 /(_))_ )(_))  )\  '/((_)   ((_)(_))\ /((_|()\  
+	(_)) __((_)_ _((_))(_))    / _ \_)((_|_))  ((_) 
+	  | (_ / _` | '  \() -_)  | (_) \ V // -_)| '_| 
+	   \___\__,_|_|_|_|\___|   \___/ \_/ \___||_|   
+                                                
+				"""
+			print "\033[1;41mYou have no more chips. Get out!\033[1;m"
+			exit() # exit out of the game because player is a loser 
+	def win(): # function for when player wins
+		player.chips+=bet # adds however much you bet to the total about of chip
+		print """
+
+	Y)    yy  O)oooo  U)    uu    W)      ww I)iiii N)n   nn 
+	 Y)  yy  O)    oo U)    uu    W)      ww   I)   N)nn  nn 
+	  Y)yy   O)    oo U)    uu    W)  ww  ww   I)   N) nn nn 
+	   Y)    O)    oo U)    uu    W)  ww  ww   I)   N)  nnnn 
+	   Y)    O)    oo U)    uu    W)  ww  ww   I)   N)   nnn 
+	   Y)     O)oooo   U)uuuu      W)ww www  I)iiii N)    nn 
+	                                                         
+				                                                         
+				"""
+		print "You gained \033[1;32m{}\033[1;m chips. You currently have \033[1;32m{}\033[1;m\n".format(bet, player.chips)
+
+	the_game()
+	answer = raw_input("Do you want to play again? (Y/N)")
+	while answer.upper()!='Y' and answer.upper()!='N': 
+		answer = raw_input("Please enter Y or N: ")
+
+#####
+
+# def start_game(): # start game function
+# 	myDeck.create() # creates the deck
+# 	myDeck.shuffle() # shuffles the deck
+# 	myDeck.deal(player, dealer) # deals the cards to the players and prints message 
+
+# def lose(): # function for when you lose
+# 	player.chips-=player.bet # you lose however much you bet
+# 	print """
+#  __   _____  _   _   _     ___  ____  _____     _______           _______  
+#  \ \ / / _ \| | | | | |   / _ \/ ___|| ____|   / /_   _|         |_   _\ \ 
+#   \ V / | | | | | | | |  | | | \___ \|  _|    | |  | |             | |  | |
+#    | || |_| | |_| | | |__| |_| |___) | |___   | |  | |             | |  | |
+#    |_| \___/ \___/  |_____\___/|____/|_____|  | |  |_|    _____    |_|  | |
+#                                                \_\       |_____|       /_/ 
+# """
+# 	print "You lost \033[1;31m{}\033[1;m chips. You currently have \033[1;31m{}\033[1;m\n".format(player.bet, player.chips)
+# 	if player.chips<=0: # if loser run out of chips
+# 		print """
+#                               )                 
+#  (                         ( /(                 
+#  )\ )      )    )     (    )\())  )     (  (    
+# (()/(   ( /(   (     ))\  ((_)\  /((   ))\ )(   
+#  /(_))_ )(_))  )\  '/((_)   ((_)(_))\ /((_|()\  
+# (_)) __((_)_ _((_))(_))    / _ \_)((_|_))  ((_) 
+#   | (_ / _` | '  \() -_)  | (_) \ V // -_)| '_| 
+#    \___\__,_|_|_|_|\___|   \___/ \_/ \___||_|   
+                                                
+# """
+# 		print "\033[1;41mYou have no more chips. Get out!\033[1;m"
+# 		exit() # exit out of the game because player is a loser 
+# def win(): # function for when player wins
+# 	player.chips+=player.bet # adds however much you bet to the total about of chip
+# 	print """
+# Y)    yy  O)oooo  U)    uu    W)      ww I)iiii N)n   nn 
+#  Y)  yy  O)    oo U)    uu    W)      ww   I)   N)nn  nn 
+#   Y)yy   O)    oo U)    uu    W)  ww  ww   I)   N) nn nn 
+#    Y)    O)    oo U)    uu    W)  ww  ww   I)   N)  nnnn 
+#    Y)    O)    oo U)    uu    W)  ww  ww   I)   N)   nnn 
+#    Y)     O)oooo   U)uuuu      W)ww www  I)iiii N)    nn 
+                                                         
+                                                         
+# """
+# 	print "You gained \033[1;32m{}\033[1;m chips. You currently have \033[1;32m{}\033[1;m\n".format(player.bet, player.chips)
+
+# def the_game(): # function that plays the game 
+# 	myDeck.reset(player, dealer) # every time the game starts/restarts, the cards are reset
+# 	start_game() # calls the start_game function and starts the game
+# 	print "Your total value is \033[1;33m{}\033[1;m".format(player.sum) # prints out total value
+
+# 	if dealer.sum == 21 and player.sum == 21: # if both dealer and player gets a black jack initially 
+# 		print "Double black jack! You tied!"
+# 		return # exits the game
+# 	elif dealer.sum == 21: # if the dealer gets a black jack
+# 		print "Dealer got black jack! Game ends. You a loser"
+# 		lose() # calls the lose function because you lost
+# 		return 
+# 	elif player.sum == 21: # if the player gets a black jack
+# 		print "You got a black jack! Game ends."
+# 		win() # calls the win function 
+# 		return
+
+# 	answer2 = raw_input("\nDo you want to hit or stay? (H = hit; S = stay)") # asks player if they want to hit or stay
+# 	while answer2.upper() == 'H': # while the answer2 is "H" aka hit
+# 		player.hit(myDeck) # player hits 
+# 		print "Your total value is \033[1;33m{}\033[1;m".format(player.sum)
+
+# 		hand_sum = player.sum # equals the the total value of player
+# 		if hand_sum>21: # if the total is greater than 21
+# 			print "You're a Loser!"
+# 			lose() # you lose
+# 			return
+# 		else: # else 
+# 			answer2 = raw_input("\nDo you want to hit or stay? (H = hit; S = stay)") # prompts H/S question again
+# 			# if user answers H, while loop will continue
+# 			# if user answer S, then exits the while loop and goes to next code
+# 	if answer2.upper() == 'S': # when the user answers S
+# 		player.stay() 
+# 		while(dealer.sum < 17): # 
+# 			dealer.hit(myDeck)
+# 		if dealer.sum > 21: 
+# 			print "Dealer bust at \033[1;31m{}\033[1;m! Player wins!".format(dealer.sum)
+# 			win()
+# 			return
+# 		elif dealer.sum > player.sum: 
+# 			print "Dealer wins \033[1;31m{}-{}\033[1;m. You lost. Loser".format(dealer.sum, player.sum)
+# 			lose()
+# 			return
+# 		elif dealer.sum < player.sum:
+# 			print "You win \033[1;31m{}-{}\033[1;m. You're not a loser. I guess".format(player.sum, dealer.sum)
+# 			win()
+# 			return
+# 		else: 
+# 			print "You tied \033[1;31m{}-{}\033[1;m. That's weird".format(player.sum, dealer.sum)
+# 			return
+##########################
+
+
